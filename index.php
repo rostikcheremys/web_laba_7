c494879c205d1531ecfffdc0050696e6955eff66
rostikcheremys <rostikcheremys@gmail.com>
2024-11-03 17:16:18 +0200

    update

--- index.php
+++ index.php
@@ -1,21 +1,37 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="uk">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lab7</title>
-    <link rel="stylesheet" href="css/style.css">
+    <title>Погода</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <h2>Погода в містах України:</h2>
-    <img src="pogoda.php?gorod=4944">
-    <img src="pogoda.php?gorod=5053"><br><br>
-    <img src="pogoda.php?gorod=5080">
-    <img src="pogoda.php?gorod=5077"><br>
-    <h2>Погода у світі:</h2>
-    <img src="pogoda.php?gorod=4368">
-    <img src="pogoda.php?gorod=471"><br><br>
-    <img src="pogoda.php?gorod=744">
-    <img src="pogoda.php?gorod=1948"><br><br>
+    <header>
+        <h1>Погода</h1>
+    </header>
+
+    <main>
+        <h2>Погода в містах України:</h2>
+
+        <section>
+            <img src="pogoda.php?gorod=4944">
+            <img src="pogoda.php?gorod=5053"><br><br>
+            <img src="pogoda.php?gorod=5080">
+            <img src="pogoda.php?gorod=5077"><br>
+        </section>
+
+        <h2>Погода у світі:</h2>
+
+        <section>
+            <img src="pogoda.php?gorod=6355">
+            <img src="pogoda.php?gorod=471"><br><br>
+            <img src="pogoda.php?gorod=744">
+            <img src="pogoda.php?gorod=1948"><br><br>
+        </section>
+    </main>
+
+    <footer>
+        <p>made by Rostyslav Cheremys, 2024</p>
+    </footer>
 </body>
 </html>
--- pogoda.php
+++ pogoda.php
@@ -4,10 +4,15 @@ $cityCode = $_GET["gorod"];
 
 $curl = curl_init();
 $url = "http://www.gismeteo.ua/city/hourly/" . $cityCode . "/";
+
 curl_setopt($curl, CURLOPT_URL, $url);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+
 $out = curl_exec($curl);
+
+if ($out === false) die("Помилка завантаження даних: " . curl_error($curl));
+
 curl_close($curl);
 
 preg_match('/<meta name="keywords" content=".*?погода в ([^,]+)/', $out, $cityMatch);
@@ -17,35 +22,37 @@ preg_match('/"date":"(\d{2}\.\d{2}\.\d{4})/', $out, $dateMatch);
 $date = $dateMatch[1] ?? 'Дата не знайдена';
 
 preg_match_all('/<temperature-value value="([-]?\d+)"/', $out, $temperatureMatches);
-$temperatureValues = [];
-for ($i = 7; $i < 15; $i++) {
-    $temperatureValues[] = (int) ($temperatureMatches[1][$i] ?? 0);
-}
+$temperatureValues = array_slice($temperatureMatches[1], 7, 8);  // Беремо перші 8 значень
 
 $im = imagecreatetruecolor(500, 200);
+
 $black = imagecolorallocate($im, 0, 0, 0);
-$red = imagecolorallocate($im, 255, 0, 0);
 $white = imagecolorallocate($im, 255, 255, 255);
-$yellow = imagecolorallocate($im, 255, 255, 0);
+$red = imagecolorallocate($im, 255, 0, 0);
 $blue = imagecolorallocate($im, 0, 0, 255);
 
 imagefilledrectangle($im, 0, 0, 499, 199, $black);
 
 imageantialias($im, true);
 
-$moon = @imagecreatefrompng('img/moon_sm.png') ?: die("Помилка: Зображення moon_sm.png не знайдено.");
-$sun = @imagecreatefrompng('img/sun_sm.png') ?: die("Помилка: Зображення sun_sm.png не знайдено.");
-$left = @imagecreatefrompng('img/left.png') ?: die("Помилка: Зображення left.png не знайдено.");
-$center = @imagecreatefrompng('img/center.png') ?: die("Помилка: Зображення center.png не знайдено.");
-$right = @imagecreatefrompng('img/right.png') ?: die("Помилка: Зображення right.png не знайдено.");
+function loadImageOrDie($path) {
+    return @imagecreatefrompng($path) ?: null;
+}
+
+$moon = loadImageOrDie('images/moon_sm.png');
+$sun = loadImageOrDie('images/sun_sm.png');
+$left = loadImageOrDie('images/left.png');
+$center = loadImageOrDie('images/center.png');
+$right = loadImageOrDie('images/right.png');
+
+if (!$moon || !$sun || !$left || !$center || !$right) die("Помилка: Одне або більше зображень не знайдено.");
 
 imagecopy($im, $left, 105, 0, 0, 0, imagesx($left), imagesy($left));
 imagecopy($im, $center, 205, 0, 0, 0, imagesx($center), imagesy($center));
 imagecopy($im, $right, 305, 0, 0, 0, imagesx($right), imagesy($right));
-
 imagecopy($im, $moon, 25, 10, 0, 0, imagesx($moon), imagesy($moon));
-imagecopy($im, $moon, 415, 10, 0, 0, imagesx($moon), imagesy($moon));
 imagecopy($im, $sun, 225, 5, 0, 0, imagesx($sun), imagesy($sun));
+imagecopy($im, $moon, 415, 10, 0, 0, imagesx($moon), imagesy($moon));
 
 $startX = 50;
 $startY = 150;
@@ -53,12 +60,12 @@ $stepX = 50;
 $maxTemp = max($temperatureValues);
 $minTemp = min($temperatureValues);
 
-imageline($im, $startX, $startY, $startX + $stepX * 7, $startY, $blue);
+imageline($im, $startX, $startY, $startX + $stepX * 7, $startY, $white);
 
 for ($i = 0; $i < 8; $i++) {
     $hour = $i * 3;
     $x = $startX + $i * $stepX;
-    imagettftext($im, 10, 0, $x - 5, $startY + 15, $blue, $fontFile, $hour);
+    imagettftext($im, 10, 0, $x - 5, $startY + 15, $white, $fontFile, $hour);
 }
 
 for ($i = 0; $i < 7; $i++) {
@@ -66,16 +73,16 @@ for ($i = 0; $i < 7; $i++) {
     $y1 = $startY - (($temperatureValues[$i] - $minTemp) / ($maxTemp - $minTemp)) * 100;
     $x2 = $startX + ($i + 1) * $stepX;
     $y2 = $startY - (($temperatureValues[$i + 1] - $minTemp) / ($maxTemp - $minTemp)) * 100;
-    
+
     imageline($im, $x1, $y1, $x2, $y2, $red);
-    
-    imagettftext($im, 10, 0, $x1 - 5, $y1 - 5, $red, $fontFile, ($temperatureValues[$i] > 0 ? '+' : '') . $temperatureValues[$i]);
+
+    $tempColor = ($temperatureValues[$i] > 0) ? $red : $blue;
+    imagettftext($im, 10, 0, $x1 - 5, $y1 - 5, $tempColor, $fontFile, ($temperatureValues[$i] > 0 ? '+' : '') . $temperatureValues[$i]);
 }
 
-imagettftext($im, 12, 0, 120, 185, $white, $fontFile, "Погода в: $city");
+imagettftext($im, 12, 0, 120, 185, $white, $fontFile, "г. $city");
 imagettftext($im, 12, 0, 280, 185, $white, $fontFile, "$date");
 
 header('Content-Type: image/png');
 imagepng($im);
 imagedestroy($im);
-?>
